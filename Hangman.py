f83488fd778bd07226aded485e725b1a44526677
Jeremy Pitt <jeremypitt@Jeremys-MacBook-Pro.local>
2017-02-10 13:26:28 -0600

    can fill in blanks with correct guess, working on decrementing counter with wrong guess

--- Hangman.py
+++ Hangman.py
@@ -1,18 +1,27 @@
-def makeAGuess():
+import Gameboard
+
+def makeAGuess(triesRemaining):
     guess = input()
     if guess in wordGuessArray:
         for x in range(len(wordGuessArray)):
             if guess == wordGuessArray[x]:
                 wordArray[x] = guess
+                print(wordArray)
+                print("Correct!")
     else:
-        print("Wrong. Try again.")
+        triesRemaining -= 1
+        print("Wrong. Try again. You have", triesRemaining, "tries remaining.")
 
 wordToGuess = "bicycle"
 wordGuessArray = list(wordToGuess)
 wordArray = ["_", "_", "_", "_", "_", "_", "_", ]
+triesRemaining = 10
 print("Welcome to hangman!\n Please enter a letter to guess the word.")
 print(wordArray)
-while "_" in wordArray:
-    makeAGuess()
-    print(wordArray)
-print("You got it!")
+while "_" in wordArray and triesRemaining > 0:
+    makeAGuess(triesRemaining)
+    # Gameboard.printGameBoard(triesRemaining)
+if "_" not in wordArray:
+    print("You got it!")
+elif triesRemaining == 0:
+    print("Game Over")
