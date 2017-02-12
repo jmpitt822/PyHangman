4714a595f5e2a714b6ddf9eacce73f0791626d6c
Jeremy Pitt <jeremypitt@Jeremys-MacBook-Pro.local>
2017-02-12 17:38:25 -0600

    added validation to prevent duplicate input

--- Hangman.py
+++ Hangman.py
@@ -4,10 +4,16 @@ import WordPicker
 
 def makeAGuess(decrementTries):
     guess = input()
-    if guess in wordGuessArray:
+    if guess in correctGuesses or guess in wrongGuesses:
+        print("You have already guessed that letter. Try again.")
+        print("Guessed Letters:", wrongGuesses)
+        print(wordArray)
+        return decrementTries
+    elif guess in wordGuessArray:
         for x in range(len(wordGuessArray)):
             if guess == wordGuessArray[x]:
                 wordArray[x] = guess
+        correctGuesses.append(guess)
         print("Guessed Letters:", wrongGuesses)
         print(wordArray)
         print("Correct!")
@@ -27,6 +33,7 @@ wordGuessArray = list(wordToGuess)
 wordArray = WordPicker.randomWordArray(wordToGuess)
 triesRemaining = 10
 wrongGuesses = []
+correctGuesses = []
 print("Welcome to hangman!\nPlease enter a letter to guess the word.")
 print(wordArray)
 Gameboard.printGameBoard(triesRemaining)
@@ -36,4 +43,4 @@ while "_" in wordArray and triesRemaining is not None:
 if "_" not in wordArray:
     print("You got it!")
 elif triesRemaining is None:
-    print("Game Over! The word was ", wordToGuess, ".", end="")
+    print("Game Over! The word was ", wordToGuess, ".")
